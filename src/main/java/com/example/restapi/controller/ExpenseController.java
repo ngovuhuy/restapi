d4b7c318abbe8580eaed35363816a68d6c2ff75f
ngovuhuy <ngovuhuy2016@gmail.com>
2024-11-19 23:24:42 +0700

    add cors

--- src/main/java/com/example/restapi/controller/ExpenseController.java
+++ src/main/java/com/example/restapi/controller/ExpenseController.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,6 +22,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @RestController
 @Slf4j
+@CrossOrigin("*")
 public class ExpenseController {
 
 	@Autowired
